57ea612a891873e7eb251216af66287deb1cd6b4
Fergal7 <f.j.mccollam@student.rug.nl>
2022-05-19 17:07:34 +0200

    you can now login users SUUIIIII

--- web/routes/auth/login.js
+++ web/routes/auth/login.js
@@ -1,6 +1,8 @@
 import express from "express";
 import config from "../../../lib/config.js";
-import { db } from "../../server.js";
+import { db, auth} from "../../server.js";
+import {  signInWithEmailAndPassword } from "firebase/auth";
+
 
 const router = express.Router(),
     Log = config.getLog("login");
@@ -9,9 +11,36 @@ router.get('/', (req, res) => {
     res.status(200).send("Request received");
 })
 
-router.post('/', (req, res) => {
-    res.status(200).send("Request received");
-})
+router.post('/', async (req, res) => {
+
+    const credentials = req.body,
+    email = credentials.email,
+    password = credentials.password;
+
+    var code,message;
+
+    try {
+       
+        const userCredential = await signInWithEmailAndPassword(auth, email, password);
+
+        const user = userCredential.user;
+        req.user = user;
+
+        code = 200;
+        message = user;
+
+        res.status(code).send(message);
+        console.log(req.body);
+        
+    }catch(error) {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        code = 400;
+        message = {errorCode, errorMessage};
+
+        res.status(code).send(message);
+      }
+});
 
 export default {
     router: router
