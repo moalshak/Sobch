1cdc3b667838e318c8f6b73e81264c25aa671966
Carmen <m.c.jica@student.rug.nl>
2022-05-20 12:26:18 +0200

    creating alter/{device-id} endpoints

--- web/routes/devices/alter.js
+++ web/routes/devices/alter.js
@@ -1,3 +1,4 @@
+import { set, ref, get, update, child, push } from "@firebase/database";
 import express from "express";
 import config from "../../../lib/config.js";
 import { db } from "../../server.js";
@@ -5,14 +6,53 @@ import { db } from "../../server.js";
 const router = express.Router(),
     Log = config.getLog("alter");
 
-router.put('/', (req, res) => {
-    res.status(200).send("Request received");
-    Log.info(`Request received`)
-})
+router.put('/:deviceId', (req, res) => {
+    const device = req.body;
+    const deviceId = req.params.deviceId;
 
-router.delete('/', (req, res) => {
-    res.status(200).send("Request received");
-    Log.info(`Request received`)
+    get(ref(db, `devices/${deviceId}`)).then((deviceSnapshot) => {
+        if(deviceSnapshot.exists()) {
+            update(ref(db, `devices/${deviceId}`), {
+                "id": device.id,
+                "config": {
+                    "min": device.config.min,
+                    "max": device.config.max,
+                    "room": device.config.room,
+                    "active": device.config.active
+                    }
+            }).then(() => {
+                res.status(200).send(`Success - device information changed`);
+                Log.info(`Success - device information changed`)
+            }).catch((error) => {
+                res.status(500).send("Internal server error");
+                Log.info("Internal server error");
+            });
+        } else {
+            res.status(401).send("Unauthorised"); 
+            Log.info('Unauthorised')
+        }
+    }).catch((error) => {
+        res.status(500).send("Internal server error");
+        Log.info("Internal server error");
+    })
+});
+
+router.delete('/:deviceId', (req, res) => {
+    const deviceId = req.params.deviceId;
+
+    get(ref(db, `devices/${deviceId}`)).then((deviceSnapshot) => {
+        if(deviceSnapshot.exists()) {
+            set(ref(db, `devices/${deviceId}`), null);
+            res.status(200).send(`Success - device unlinked`);
+            Log.info(`Success - device unlinked`)
+        } else {
+            res.status(401).send("Unauthorised"); 
+            Log.info('Unauthorised')
+        }
+    }).catch((error) => {
+        res.status(500).send("Internal server error");
+        Log.info("Internal server error");
+    })
 })
 
 export default {
