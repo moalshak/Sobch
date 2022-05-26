5a9199004fea34bca80fa489579d6ee40f43302c
Dhruv Ghosh <dhruvghosh@dhruvs-mbp.kpn>
2022-05-26 14:06:02 +0200

    I love mo, my-devices get request for all devices lost

--- web/routes/devices/my-devices.js
+++ web/routes/devices/my-devices.js
@@ -36,26 +36,31 @@ router.post("/", async (req, res) => {
     if (snapshot.exists()) {
         var deviceSnapshot = snapshot.val();
         // otp matches -> add device
+
         if (otp === deviceSnapshot.otp) {
             Log.info("OTP MATCHES!")
             // make sure device owners is an array
             if (deviceSnapshot.owners === undefined || !Array.isArray(deviceSnapshot.owners)) {
                 deviceSnapshot.owners = [];
             }
+
             // device already owned
             var deviceLiked = deviceSnapshot.owners.includes(user.uid);
             if (!deviceLiked) {
                 deviceSnapshot.owners.push(user.uid);
             }
+
             await set(ref(db, `devices/${deviceId}`), deviceSnapshot);
             var userSnapshot = (await get(ref(db, `users/${user.uid}`))).val();
             if (userSnapshot.owns === undefined || !Array.isArray(userSnapshot.owns)) {
                 userSnapshot.owns = [];
             }
+
             var userLinked = userSnapshot.owns.includes(deviceId);
             if (!userLinked) {
                 userSnapshot.owns.push(deviceId);
             }
+
             await set(ref(db, `users/${user.uid}`), userSnapshot);
 
             if (deviceLiked && userLinked) {
@@ -77,16 +82,36 @@ router.post("/", async (req, res) => {
     }
 });
 
-router.get("/", (req, res) => {
-    // TODO : send back the access token : {accessToken: req.user.stsTokenManager.accessToken}
-    // TODO: only allow this if the user is the owner of the device
-    const user = req.user;
+// router.get("/", (req, res) => {
+//     // TODO : send back the access token : {accessToken: req.user.stsTokenManager.accessToken}
+//     // TODO: only allow this if the user is the owner of the device
+//     const user = req.user;
 
-    var fuck = get(ref(db, `devices`));
-    console.log(fuck);
-    res.status(200).send("Request received");
-    Log.info("Request received");
-});
+//     var habibi = get(ref(db, `devices`));
+//     console.log(habibi);
+//     res.status(200).send("Request received");
+//     Log.info("Request received");
+// });
+
+router.get('/', (req, res) => {
+    try{
+    const user = req.user;
+    } catch(error) {
+        res.status(400).send({error: "Bad Request"});
+        Log.error(error);
+        return;
+    }
+    //using this 'user' variable for now.
+    get(ref(db, `users/${user.uid}`)).then((snapshot) => {
+            if (snapshot.exists())
+            {
+                res.status(200).send({message: "Devices", deviceID: snapshot.val().owns, accessToken: req.user.stsTokenManager.accessToken})
+            }
+      }).catch((error) => {
+        console.error(error);
+      });
+        
+  })
 
 export default {
     router: router
