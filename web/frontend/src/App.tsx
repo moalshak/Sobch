051331b697841f436f35a40f688409322de7548a
Mohammad <mo.alshakoush@gmail.com>
2022-05-26 18:04:05 +0200

    added skeleton for whole front end with init for all routes

--- web/frontend/src/App.tsx
+++ web/frontend/src/App.tsx
@@ -1,58 +1,36 @@
-import {useState} from 'react';
 import './App.css';
-import axios from 'axios';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Register from './components/Register';
+import Login from './components/Login';
+import Home from './components/Home';
+import Profile from './components/Profile';
+import Logout from './components/Logout';
+import MyDevices from './components/MyDevices';
+import Stats from './components/Stats';
+import Alter from './components/Alter';
 
-export const BASE_URL = "http://localhost:8000"
+
+export const BACKEND_BASE_URL = "http://localhost:8000"
 // export const BASE_URL = "http://192.168.2.23:8000"
 
 function App() {
 
   return (
-    <Register/>
-  )
-
-  const [deviceId, setDeviceId] = useState('');
-  const [min, setMin] = useState(0);
-  const [max, setMax] = useState(100);
-  const [room, setRoom] = useState('');
-  const [active, setActive] = useState(true);
-  const [otp, setOtp] = useState('');
-
-  // do a post request to http://localhost:8080/my-devices using axios
-  async function dowhat(e : any) {
-    e.preventDefault();
-
-    try {
-      await axios.post('http://localhost:8000/my-devices', {
-        deviceId,
-        min,
-        max,
-        room,
-        active,
-        otp,
-      });
-
-    } catch(error) {
-      console.log(error);
-    }
-  }
-
-  return (
-    <div className="App">
-      {/* a form that has 6 inputs. device-id, min, max, room, active, otp */}
-      {/* the form does a post request to "localhost:8080/my-devices" */}
-      <form onSubmit={dowhat}>
-        <input type="text" value={deviceId} onChange={(e) => setDeviceId(e.target.value) } placeholder="Device ID" />
-        <input type="number" value={min} onChange={(e) => setMin(parseInt(e.target.value)) } placeholder="Min" />
-        <input type="number" value={max} onChange={(e) => setMax(parseInt(e.target.value)) } placeholder="Max" />
-        <input type="text" value={room} onChange={(e) => setRoom(e.target.value) } placeholder="Room" />
-        <input type="text" value={active ? 'Yes' : 'No'} onChange={(e) => setActive(e.target.value === 'Yes' ? true : false) } placeholder="Active" />
-        <input type="text" value={otp} onChange={(e) => setOtp(e.target.value) } placeholder="OTP" />
-        <button type="submit">Submit</button>
-      </form>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/profile/:id" element={<Profile />} />
+        <Route path="/logout" element={<Logout />} />
+        <Route path="/my-devices" element={<MyDevices />} />
+        <Route path="/stats/:deviceId" element={<Stats />} />
+        <Route path="/alter/:deviceId" element={<Alter />} />
+      </Routes>
+    </Router>
   );
 }
 
-export default App;
+export {
+  App
+};
