ee9a12daca44415a66f727a200abd49c92c31723
Fergal7 <f.j.mccollam@student.rug.nl>
2022-05-26 21:26:09 +0200

    added/fixed missing image from last push

--- web/frontend/src/components/Home.tsx
+++ web/frontend/src/components/Home.tsx
@@ -3,7 +3,9 @@ import {Link} from "react-router-dom"
 function Home() {
     return (
         <div>
-            
+            <img  src="Sobchlogo.png"
+            width = "200"
+            height = "100"/>
             <h1>Home</h1>
             <Link to={'/Login'}>
             <button>Login</button>
