a71b22e86d39ca6d2c9ead2f35f2ec0b132ac2ab
Selim <selimaigle@gmail.com>
2022-05-26 14:53:43 +0200

    Finished the profile endpoints and the verification emails

--- web/routes/auth/login.js
+++ web/routes/auth/login.js
@@ -1,7 +1,7 @@
 import express from "express";
 import config from "../../../lib/config.js";
 import { db, auth} from "../../server.js";
-import {  signInWithEmailAndPassword } from "firebase/auth";
+import {  signInWithEmailAndPassword, sendEmailVerification } from "firebase/auth";
 
 
 const router = express.Router(),
@@ -19,6 +19,8 @@ router.post('/', async (req, res) => {
 
     var code,message;
 
+    
+
 
     try {
         const userCredential = await signInWithEmailAndPassword(auth, email, password);
@@ -29,7 +31,22 @@ router.post('/', async (req, res) => {
 
         code = 200;
         message = "Success - Logged in"
-        res.status(code).send({accessToken: user.stsTokenManager.accessToken});
+        if(!user.emailVerified){
+            sendEmailVerification(user)
+            .then(() => {
+                console.log("Verification email has been sent")
+                message = "Verify email in order to login"
+                code = 401;
+                res.status(code).send({message});
+            }).catch((error) => {
+                console.error(error);
+                res.status(400).send({error : error});
+            });
+        }else {
+            res.status(code).send({accessToken: user.stsTokenManager.accessToken, message});
+        }
+
+        //console.log(user);
     } catch(error) {
          const errorCode = error.code;
          var message = {error : ""}
