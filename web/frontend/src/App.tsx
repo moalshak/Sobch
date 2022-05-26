7738fdd9c1aec1e5a7b4561fef95878a9e4d50e1
Fergal7 <f.j.mccollam@student.rug.nl>
2022-05-26 18:43:22 +0200

    Login frontend

--- web/frontend/src/App.tsx
+++ web/frontend/src/App.tsx
@@ -10,7 +10,7 @@ import Stats from './components/Stats';
 import Alter from './components/Alter';
 
 
-export const BACKEND_BASE_URL = "http://localhost:8000"
+const BACKEND_BASE_URL = "http://localhost:8000"
 // export const BASE_URL = "http://192.168.2.23:8000"
 
 function App() {
@@ -32,5 +32,6 @@ function App() {
 }
 
 export {
-  App
+  App,
+  BACKEND_BASE_URL
 };
