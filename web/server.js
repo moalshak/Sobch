84a41c09efcce4d083f26eb86d413805dacc569d
Mohammad <mo.alshakoush@gmail.com>
2022-06-02 13:35:16 +0200

    addming subdomain

--- web/server.js
+++ web/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import {PORT, getLog, getUser} from '../lib/config.js';
 import {db, auth, admin_auth, app} from '../lib/firebase.js';
+import subdomain from 'express-subdomain';
 
 /**
  * the routes
