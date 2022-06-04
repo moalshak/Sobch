950828b1442ec3d43f07ce8425224e8f083f2dcb
Fergal7 <f.j.mccollam@student.rug.nl>
2022-06-04 16:28:02 +0200

    revert

--- web/frontend/src/components/TeamSobch.tsx
+++ web/frontend/src/components/TeamSobch.tsx
@@ -25,11 +25,63 @@ function Team(){
                         
         <br />
         </div>
-        </div>
-        </div>
+
+        <div className="col-6 col-sm-4">
+            <Card border="dark" style={{ width: '18rem' }}>
+                <Card.Header>Fergal</Card.Header>
+                    <Card.Body>
+                    <Card.Text>
+                        <img src="./images/me3.jpeg" className="img-thumbnail"></img>
+                        <Card.Footer>Project Leader</Card.Footer>
+                    </Card.Text>
+                    </Card.Body>
+            </Card>
+        <br />
         </div>
 
-       
+        <div className="col-6 col-sm-4">
+            <Card border="dark" style={{ width: '18rem' }}>
+                <Card.Header>Carmen</Card.Header>
+                    <Card.Body>
+                    <Card.Text>
+                        <img src="./images/me.jpg" className="img-thumbnail"></img>
+                        <Card.Footer>Architecture Officer</Card.Footer>
+                    </Card.Text>
+                    </Card.Body>
+            </Card>
+        <br />
+        </div>
+
+        <div className="col d-flex justify-content-center">
+            <div className="col-6 col-sm-4">
+                <Card border="dark" style={{ width: '18rem' }}>
+                    <Card.Header>Selim</Card.Header>
+                        <Card.Body>
+                        <Card.Text>
+                            <img src="./images/me.jpg" className="img-thumbnail"></img>
+                            <Card.Footer>Documentation Officer</Card.Footer>
+                        </Card.Text>
+                        </Card.Body>
+                </Card>
+        <br />
+        </div>
+
+        <div className="col-6 col-sm-4">
+            <Card border="dark" style={{ width: '18rem' }}>
+                <Card.Header>Dhruv</Card.Header>
+                <Card.Body>
+                <Card.Text>
+                    <img src="./images/dhruv.jpg" className="img-thumbnail"></img>
+                    <Card.Footer>Communication Officer</Card.Footer>
+                </Card.Text>
+                </Card.Body>
+                </Card>
+        <br />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
 
         <NavBarBot />
         </div> 
