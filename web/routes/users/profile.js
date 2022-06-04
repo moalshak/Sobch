27b346343a37d3cec435b14ee69ba98716fe2a0b
Mohammad <mo.alshakoush@gmail.com>
2022-06-04 16:14:02 +0200

    admins profile view for others

--- web/routes/users/profile.js
+++ web/routes/users/profile.js
@@ -10,47 +10,41 @@ const router = express.Router(),
 router.get('/:id', (req, res) => {
     if (!req.user) {
         return res.status(401).send({error : "Unauthorized access"});
-    } else if (req.user.uid !== req.params.id) {
+    } else if (!isAdmin(requester) || req.user.uid !== req.params.id) {
         return res.status(401).send({error : "Unauthorized access"});
     }
 
     const requester = req.user.uid;
     const requested = req.params.id;
+    const meta = req.user.metadata;
 
-    if (!isAdmin(requester)) {
-        const meta = req.user.metadata;
-    
-        get(ref(db, `users/${requested}`)).then((snapshot) => {
-            if (!snapshot.exists()) {
-                set(ref(db, `users/${requested}`), {
-                    credentials : {
-                        email : ""
-                    }
-                }).then(() => {
-                    if (requester && requested) {
-                        res.status(200).send({profile: snapshot.val(), accessToken: req.user.stsTokenManager.accessToken, meta});
-                        Log.info("Profile details returned successfully ", {requester, requested});
-                        return;
-                    } else if (!snapshot.exists()){
-                        res.status(401).send({error : "Unauthorized access"});
-                        return;
-                    } else {
-                        res.status(400).send({error : "Bad Request"});
-                        return;
-                    }
-                });
-            }
-        }).catch((error) => {
-            console.error(error);
-            res.status(401).send({error : true, message : "Unauthorized access"});
-            return;
-        });        
-    } else {
-        Log.error(error);
+    get(ref(db, `users/${requested}`)).then((snapshot) => {
+        if (!snapshot.exists()) {
+            set(ref(db, `users/${requested}`), {
+                credentials : {
+                    email : ""
+                }
+            }).then(() => {
+                if (requester && requested) {
+                    res.status(200).send({profile: snapshot.val(), accessToken: req.user.stsTokenManager.accessToken, meta});
+                    Log.info("Profile details returned successfully ", {requester, requested});
+                    return;
+                } else if (!snapshot.exists()){
+                    res.status(401).send({error : "Unauthorized access"});
+                    return;
+                } else {
+                    res.status(400).send({error : "Bad Request"});
+                    return;
+                }
+            });
+        }
+    }).catch((error) => {
+        console.error(error);
         res.status(401).send({error : true, message : "Unauthorized access"});
         return;
-    }
+    });        
 })
+
 
 router.get('/', async (req, res) => {
     //to do : testinggggg
